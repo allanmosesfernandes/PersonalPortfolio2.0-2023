7db8968cbc2b178dfa6c7128e3188ad2e4b7f0a8
Allan <fernzallan14@gmail.com>
2023-06-14 12:46:10 +0100

    Normalize

--- portfolio23/src/pages/index.js
+++ portfolio23/src/pages/index.js
@@ -1,5 +1,7 @@
-import * as React from "react"
+import * as React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import 'normalize.css';
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <div>Hello world!</div>;
 }
