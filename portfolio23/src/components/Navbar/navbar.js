8ad34bc267ae1ea65ac8834d4cefffbf9eb71269
Allan Fernandes <Fernzallan14@gmail.com>
2023-06-14 21:46:51 +0100

    Header string responsive

--- portfolio23/src/components/Navbar/navbar.js
+++ portfolio23/src/components/Navbar/navbar.js
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyledNavbar } from './navbarStyled';
 
 function Navbar() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    if (window.innerWidth <= 768) {
+      setIsMobile(true);
+    }
+    handleResize(); // call it initially to initialize the state
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <StyledNavbar>
-      <h2>
-        <span>{"<"}</span>
-        AllanFernandes
-        <span>{"/>"}</span>
-      </h2>
+      {isMobile
+        ? (
+          <h2>
+            <span>{'<'}</span>
+            AllanF
+            <span>{'/>'}</span>
+          </h2>
+        )
+        : (
+          <h2>
+            <span>{'<'}</span>
+            AllanFernandes
+            <span>{'/>'}</span>
+          </h2>
+        )}
     </StyledNavbar>
   );
 }
