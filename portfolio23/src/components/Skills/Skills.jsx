25267cedf9424de6771631815676f834d547a03a
Allan Fernandes <Fernzallan14@gmail.com>
2023-06-21 17:06:15 +0100

    Icons

--- portfolio23/src/components/Skills/Skills.jsx
+++ portfolio23/src/components/Skills/Skills.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { SkillsContainer } from "./skillsStyles";
+import { SkillsContainer, TechGrid } from "./skillsStyles";
 import Asterik from "../../assets/images/home/asterik.svg";
 
 function Skills() {
@@ -10,12 +10,64 @@ function Skills() {
         Skills & Tools
       </div>
       <p>
-        <img src={Asterik} alt="Asterik" className='ill'/>
+        <img src={Asterik} alt="Asterik" className="ill" />
         Programming languages, frameworks, tools & products I've either had
         hands-down experience with or I've worked on and tried out at least once
         and would not call myself a Pro. I'm always looking to learn new things
         and improve my skills.
       </p>
+      <TechGrid>
+        <div class="terminal-panel">
+          <svg
+            viewBox="0 0 512 512"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="#FF605C"
+            width="10"
+            height="10"
+          >
+            <path d="m512 256c0 141.386719-114.613281 256-256 256s-256-114.613281-256-256 114.613281-256 256-256 256 114.613281 256 256zm0 0"></path>
+            <path d="m384 256c0 70.691406-57.308594 128-128 128s-128-57.308594-128-128 57.308594-128 128-128 128 57.308594 128 128zm0 0"></path>
+            <title>close</title>
+          </svg>
+          <svg
+            viewBox="0 0 512 512"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="#FFBD44"
+            width="10"
+            height="10"
+          >
+            <path d="m512 256c0 141.386719-114.613281 256-256 256s-256-114.613281-256-256 114.613281-256 256-256 256 114.613281 256 256zm0 0"></path>
+            <path d="m384 256c0 70.691406-57.308594 128-128 128s-128-57.308594-128-128 57.308594-128 128-128 128 57.308594 128 128zm0 0"></path>
+            <title>minimise</title>
+          </svg>
+          <svg
+            viewBox="0 0 512 512"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="#00CA4E"
+            width="10"
+            height="10"
+          >
+            <path d="m512 256c0 141.386719-114.613281 256-256 256s-256-114.613281-256-256 114.613281-256 256-256 256 114.613281 256 256zm0 0"></path>
+            <path d="m384 256c0 70.691406-57.308594 128-128 128s-128-57.308594-128-128 57.308594-128 128-128 128 57.308594 128 128zm0 0"></path>
+            <title>maximize </title>
+          </svg>
+        </div>
+        <div className="tech-grid__item">
+          <img />
+        </div>
+        <div className="tech-grid__item">
+          <img />
+        </div>
+        <div className="tech-grid__item">
+          <img />
+        </div>
+        <div className="tech-grid__item">
+          <img />
+        </div>
+        <div className="tech-grid__item">
+          <img />
+        </div>
+      </TechGrid>
     </SkillsContainer>
   );
 }
--- portfolio23/src/components/Skills/skillsStyles.js
+++ portfolio23/src/components/Skills/skillsStyles.js
@@ -1,14 +1,15 @@
 import styled from "styled-components";
-import Grainy from "../../assets/images/home/bg-grain-dark.png";
+import Grainy from "../../assets/images/home/bg-grain.png";
 
 export const SkillsContainer = styled.div`
   margin: 2rem auto;
+  background-color: rgb(255, 255, 255);
   background-image: url(${Grainy});
-  background-color: rgba(0, 0, 0, 0.8);
   background-attachment: fixed;
   border-radius: 10px;
   font-family: "Fira Code", monospace;
   padding: 2rem;
+  color: #000;
   .skills__title {
     font-size: var(--fs4);
     display: flex;
@@ -25,7 +26,7 @@ export const SkillsContainer = styled.div`
       content: "";
       width: 50%;
       height: 2px;
-      background-color: white;
+      background-color: black;
     }
     img {
       &:after {
@@ -51,14 +52,26 @@ export const SkillsContainer = styled.div`
 `;
 
 export const TechGrid = styled.div`
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    grid-gap: 2rem;
-    margin: 2rem 0;
-    @media only screen and (max-width: 768px) {
-        grid-template-columns: repeat(2, 1fr);
-    }
-    @media only screen and (max-width: 500px) {
-        grid-template-columns: repeat(1, 1fr);
-    }
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 2rem;
+  margin: 2rem 0;
+  margin-left: 10rem;
+  border: 2px solid #121212;
+  padding: 10px;
+  padding-top: 5px;
+  -webkit-box-shadow: 4px 4px 0 #121217;
+  box-shadow: 4px 4px 0 #121217;
+  max-width: 700px;
+  min-height: 250px;
+  @media only screen and (max-width: 768px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+  @media only screen and (max-width: 500px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
+  .terminal-panel {
+    display: flex;
+    gap: 1rem;
+  }
 `;
