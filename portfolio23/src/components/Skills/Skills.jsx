197080978a5f5e092bbafaad674edabf49781d58
Allan Fernandes <Fernzallan14@gmail.com>
2023-06-21 15:29:11 +0100

    Skills-Init

---
+++ portfolio23/src/components/Skills/Skills.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SkillsContainer } from "./skillsStyles";
+
+function Skills() {
+  return (
+    <SkillsContainer className='wrapper'>
+        <h2>kj</h2>
+    </SkillsContainer>
+  );
+}
+
+export default Skills;
--- portfolio23/src/pages/index.js
+++ portfolio23/src/pages/index.js
@@ -2,9 +2,13 @@ import * as React from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import 'normalize.css';
 import FirstFold from '../components/FirstFold/FirstFold';
+import Skills from '../components/Skills/Skills';
 
 export default function Home() {
   return (
-    <FirstFold />
+    <>
+      <FirstFold />
+      <Skills />
+    </>
   );
 }
