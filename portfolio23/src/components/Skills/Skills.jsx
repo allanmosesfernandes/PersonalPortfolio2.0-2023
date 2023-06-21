d70ee4a3fa64c0e51882d66b44d053646db905f1
Allan Fernandes <Fernzallan14@gmail.com>
2023-06-21 16:49:40 +0100

    Skills-Title

--- portfolio23/src/components/Skills/Skills.jsx
+++ portfolio23/src/components/Skills/Skills.jsx
@@ -9,6 +9,13 @@ function Skills() {
         <img src={Asterik} alt="Asterik" />
         Skills & Tools
       </div>
+      <p>
+        <img src={Asterik} alt="Asterik" className='ill'/>
+        Programming languages, frameworks, tools & products I've either had
+        hands-down experience with or I've worked on and tried out at least once
+        and would not call myself a Pro. I'm always looking to learn new things
+        and improve my skills.
+      </p>
     </SkillsContainer>
   );
 }
--- portfolio23/src/components/Skills/skillsStyles.js
+++ portfolio23/src/components/Skills/skillsStyles.js
@@ -4,26 +4,61 @@ import Grainy from "../../assets/images/home/bg-grain-dark.png";
 export const SkillsContainer = styled.div`
   margin: 2rem auto;
   background-image: url(${Grainy});
+  background-color: rgba(0, 0, 0, 0.8);
   background-attachment: fixed;
   border-radius: 10px;
-
+  font-family: "Fira Code", monospace;
+  padding: 2rem;
   .skills__title {
     font-size: var(--fs4);
-    font-family: "Fira Code", monospace;
     display: flex;
     align-items: center;
     gap: 2rem;
-    padding: 2rem;
     //On hover rotate the asterik icon
     &:hover {
-        img {
-            transform: rotate(180deg);
-            transition: transform 0.5s ease-in-out;
-        }
+      img {
+        transform: rotate(180deg);
+        transition: transform 0.5s ease-in-out;
+      }
+    }
+    &:after {
+      content: "";
+      width: 50%;
+      height: 2px;
+      background-color: white;
+    }
+    img {
+      &:after {
+        content: "";
+        width: 50%;
+        height: 2px;
+        background-color: white;
+      }
+    }
   }
-}
+
+  p {
+    font-size: var(--fs2);
+    line-height: 1.5;
+    width: 75%;
+    display: flex;
+    gap: 2rem;
+    .ill {
+      opacity: 0;
+      display: inline-block;
+    }
+  }
 `;
 
-export const lodaLasson = styled.div`
-    background-color: #ffffff;
+export const TechGrid = styled.div`
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 2rem;
+    margin: 2rem 0;
+    @media only screen and (max-width: 768px) {
+        grid-template-columns: repeat(2, 1fr);
+    }
+    @media only screen and (max-width: 500px) {
+        grid-template-columns: repeat(1, 1fr);
+    }
 `;
