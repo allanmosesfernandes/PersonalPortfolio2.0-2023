15705527baaa279a83053e98358af8bea4b72409
Allan Fernandes <Fernzallan14@gmail.com>
2023-06-21 18:56:25 +0100

    Icons-new

--- portfolio23/src/components/Skills/Skills.jsx
+++ portfolio23/src/components/Skills/Skills.jsx
@@ -1,6 +1,24 @@
-import React from 'react';
+import React from "react";
 import { SkillsContainer, TechGrid, Terminal } from "./skillsStyles";
 import Asterik from "../../assets/images/home/asterik.svg";
+import red from "../../assets/images/skills/buttons/red.svg";
+import orange from "../../assets/images/skills/buttons/orange.svg";
+import green from "../../assets/images/skills/buttons/green.svg";
+import html from "../../assets/images/skills/html-5.svg";
+import css3 from "../../assets/images/skills/css-3.svg";
+import javascript from "../../assets/images/skills/javascript.svg";
+import react from "../../assets/images/skills/react.svg";
+import jquery from "../../assets/images/skills/jquery.svg";
+import sass from "../../assets/images/skills/sass.svg";
+import styledIcon from "../../assets/images/skills/styled-components-1.svg";
+import bootstrap from "../../assets/images/skills/bootstrap.svg";
+import gatsbyIcon from "../../assets/images/skills/gatsby.svg";
+import githubIcon from "../../assets/images/skills/github-icon.svg";
+import wordpress from "../../assets/images/skills/wordpress-icon.svg";
+import netlify from "../../assets/images/skills/netlify.svg";
+import sanity from "../../assets/images/skills/sanity.svg";
+import firebase from "../../assets/images/skills/firebase.svg";
+import figma from "../../assets/images/skills/figma.svg";
 
 function Skills() {
   return (
@@ -16,26 +34,81 @@ function Skills() {
         and would not call myself a Pro. I'm always looking to learn new things
         and improve my skills.
       </p>
-      <TechGrid>
-        <Terminal>
 
-        </Terminal>
-        <div className="tech-grid__item">
-          <img />
+      <Terminal>
+        <div className="terminal-window">
+          <img src={red} alt="red" />
+          <img src={orange} alt="orange" />
+          <img src={green} alt="green" />
         </div>
-        <div className="tech-grid__item">
-          <img />
-        </div>
-        <div className="tech-grid__item">
-          <img />
-        </div>
-        <div className="tech-grid__item">
-          <img />
-        </div>
-        <div className="tech-grid__item">
-          <img />
-        </div>
-      </TechGrid>
+        <TechGrid>
+          <div className="tech-grid__item">
+            <img src={html} alt="HTML-5" />
+            <p>HTML5</p>
+          </div>
+          <div className="tech-grid__item">
+            <img src={css3} alt="" />
+            <p>CSS</p>
+          </div>
+          <div className="tech-grid__item">
+            <img src={javascript} alt="HTML-5" />
+            <p>Javascript</p>
+          </div>
+          <div className="tech-grid__item">
+            <img src={react} alt="HTML-5" />
+            <p>React</p>
+          </div>
+          <div className="tech-grid__item">
+            <img src={gatsbyIcon} alt="HTML-5" />
+            <p>Gatsby</p>
+          </div>
+          <div className="tech-grid__item">
+            <img src={styledIcon} alt="HTML-5" />
+            <p>Styled Components</p>
+          </div>
+          <div className="tech-grid__item">
+            <img src={sass} alt="HTML-5" />
+            <p>Sass</p>
+          </div>
+          <div className="tech-grid__item">
+            <img src={bootstrap} alt="HTML-5" />
+            <p>Bootstrap</p>
+          </div>
+          <div className="tech-grid__item">
+            <img src={githubIcon} alt="HTML-5" />
+            <p>Github</p>
+          </div>
+          <div className="tech-grid__item">
+            <img src={wordpress} alt="HTML-5" />
+            <p>Wordpress</p>
+          </div>
+          <div className="tech-grid__item">
+            <img src={figma} alt="HTML-5" />
+            <p>Figma</p>
+          </div>
+          <div className="tech-grid__item">
+            <img src={firebase} alt="HTML-5" />
+            <p>Firebase</p>
+          </div>
+
+          <div className="tech-grid__item">
+            <img src={sanity} alt="HTML-5" />
+            <p>Sanity</p>
+          </div>
+          <div className="tech-grid__item">
+            <img src={jquery} alt="HTML-5" />
+            <p>Jquery</p>
+          </div>
+          <div className="tech-grid__item">
+            <img src={netlify} alt="HTML-5" />
+            <p>Netlify</p>
+          </div>
+          <div className="tech-grid__item">
+            <img src={sanity} alt="HTML-5" />
+            <p>Sanity</p>
+          </div>
+        </TechGrid>
+      </Terminal>
     </SkillsContainer>
   );
 }
--- portfolio23/src/components/Skills/skillsStyles.js
+++ portfolio23/src/components/Skills/skillsStyles.js
@@ -1,8 +1,10 @@
 import styled from "styled-components";
 import Grainy from "../../assets/images/home/bg-grain.png";
+import CursorBlack from "../../assets/images/home/cursorblack.svg";
 
 export const SkillsContainer = styled.div`
   margin: 2rem auto;
+  cursor: url(${CursorBlack}), auto;
   background-color: rgb(255, 255, 255);
   background-image: url(${Grainy});
   background-attachment: fixed;
@@ -57,17 +59,40 @@ export const Terminal = styled.div`
   padding-top: 5px;
   -webkit-box-shadow: 4px 4px 0 #121217;
   box-shadow: 4px 4px 0 #121217;
-  max-width: 700px;
+  width: 70%;
+  min-width: 500px;
   min-height: 250px;
   margin: 2rem 0;
-  
+  grid-gap: 2rem;
+  margin-left: 10rem;
+
+  .terminal-window {
+    display: flex;
+    gap: 1rem;
+  }
 `;
 export const TechGrid = styled.div`
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  grid-gap: 2rem;
-  margin-left: 10rem;
-  
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 1rem;
+  margin: 2rem 0;
+  img {
+    width: 60px;
+    height: 60px;
+  }
+  .tech-grid__item {
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap:0;
+    p {
+        width: 100%;
+        justify-content: center;
+        font-size: var(--fs1-5);
+    }
+  }
   @media only screen and (max-width: 768px) {
     grid-template-columns: repeat(2, 1fr);
   }
