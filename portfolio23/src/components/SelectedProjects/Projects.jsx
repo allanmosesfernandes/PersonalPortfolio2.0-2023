2b6bcf278ef29074f387faf70cf335cb9b4952b1
Allan Fernandes <Fernzallan14@gmail.com>
2023-06-22 12:15:10 +0100

    Component-Done

--- portfolio23/src/components/SelectedProjects/Projects.jsx
+++ portfolio23/src/components/SelectedProjects/Projects.jsx
@@ -15,46 +15,22 @@ function Projects() {
   return (
     <ProjectsContainer className="wrapper">
       <div className="projects__title">Some Projects</div>
-      <ProjectsWrapper>
-        {/* On hover of Div set state to true  */}
-        {/* Convert below to a component */}
-        <SingleProject
-          ProjectUrl="https://dailystoicreminders.uk/"
-          ProjectTitle="Daily Stoic Reminders"
-          onMouseEnter={() => setBlinky(true)}
-          onMouseLeave={() => setBlinky(false)}
-          ProjectImage={DailyStoic}
-          ProjectDescription="Wesbite design & development."
-        />
-        <ProjectSingle
-          onMouseEnter={() => setBlinky(true)}
-          onMouseLeave={() => setBlinky(false)}
-        >
-          {/* Rest of the code */}
-          <a
-            href="https://dailystoicreminders.uk/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <div className="block__one">
-              <p className="block_title">Daily Stoic Reminders</p>
-              <p>Wesbite design & development.</p>
-            </div>
-            <div className="block__two">
-              <img src={DailyStoic} alt="Daily Stoic" />
-            </div>
-            <div className="block__three">
-              <Marquee play={blinky}>
-                <p>
-                  {Array.from({ length: 3 }).map(
-                    (_, i) => `Daily Stoic Reminders - `
-                  )}
-                </p>
-              </Marquee>
-            </div>
-          </a>
-        </ProjectSingle>
-      </ProjectsWrapper>
+      <SingleProject
+        ProjectUrl="https://dailystoicreminders.uk/"
+        ProjectTitle="Daily Stoic Reminders"
+        onMouseEnter={() => setBlinky(true)}
+        onMouseLeave={() => setBlinky(false)}
+        ProjectImage={DailyStoic}
+        ProjectDescription="Website design & development."
+      />
+      <SingleProject
+        ProjectUrl="https://dailystoicreminders.uk/"
+        ProjectTitle="Daily Stoic Reminders"
+        onMouseEnter={() => setBlinky(true)}
+        onMouseLeave={() => setBlinky(false)}
+        ProjectImage={DailyStoic}
+        ProjectDescription="Website design & development."
+      />
     </ProjectsContainer>
   );
 }
--- portfolio23/src/components/SelectedProjects/SingleProject.jsx
+++ portfolio23/src/components/SelectedProjects/SingleProject.jsx
@@ -1,10 +1,41 @@
 import React from 'react';
+import { useState } from 'react';
 import { ProjectSingle } from "./styledProjects";
+import Marquee from 'react-fast-marquee';
 
 function SingleProject(props) {
-  const { ProjectUrl, title } = props;
+  const [blinky, setBlinky] = useState(false);
+  const {
+    ProjectUrl,
+    ProjectTitle,
+    onMouseEnter,
+    onMouseLeave,
+    ProjectDescription,
+    ProjectImage,
+  } = props;
   return (
-    <ProjectSingle>SingleProject</ProjectSingle>
+    <ProjectSingle
+      onMouseEnter={() => setBlinky(true)}
+      onMouseLeave={() => setBlinky(false)}
+    >
+      {/* Rest of the code */}
+      <a href={ProjectUrl} target="_blank" rel="noopener noreferrer">
+        <div className="block__one">
+          <p className="block_title">{ProjectTitle}</p>
+          <p>{ProjectDescription}</p>
+        </div>
+        <div className="block__two">
+          <img src={ProjectImage} alt="Daily Stoic" />
+        </div>
+        <div className="block__three">
+          <Marquee play={blinky}>
+            <p>
+              {Array.from({ length: 3 }).map((_, i) => `${ProjectTitle} - `)}
+            </p>
+          </Marquee>
+        </div>
+      </a>
+    </ProjectSingle>
   );
 }
 
