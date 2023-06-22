7b9a77460b4bd1e9c1aedb8ec146fd76217f1443
Allan Fernandes <Fernzallan14@gmail.com>
2023-06-22 23:08:21 +0100

    Footer-init

---
+++ src/components/Footer/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { FooterContainer } from './footerStyles';
+const footer = () => {
+  return (
+    <FooterContainer>
+    </FooterContainer>
+  )
+}
+
+export default footer
---
+++ src/components/Footer/footerStyles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const FooterContainer = styled.footer`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
