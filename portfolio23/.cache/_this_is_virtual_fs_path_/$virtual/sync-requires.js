273279b07769e266eca74b29175c29bdc72792a3
Allan Fernandes <Fernzallan14@gmail.com>
2023-06-22 18:31:40 +0100

    removed-directories

---
+++ portfolio23/.cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -0,0 +1,12 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("C:\\Users\\fernz\\Desktop\\code_\\2023_Projects\\June\\PersonalPortfolio2.0-2023\\portfolio23\\.cache\\dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("C:\\Users\\fernz\\Desktop\\code_\\2023_Projects\\June\\PersonalPortfolio2.0-2023\\portfolio23\\src\\pages\\404.js")),
+  "component---src-pages-about-js": preferDefault(require("C:\\Users\\fernz\\Desktop\\code_\\2023_Projects\\June\\PersonalPortfolio2.0-2023\\portfolio23\\src\\pages\\about.js")),
+  "component---src-pages-index-js": preferDefault(require("C:\\Users\\fernz\\Desktop\\code_\\2023_Projects\\June\\PersonalPortfolio2.0-2023\\portfolio23\\src\\pages\\index.js"))
+}
+
