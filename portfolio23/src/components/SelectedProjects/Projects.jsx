c800d50bbece8bda85ce84f80216c58de8227914
Allan Fernandes <Fernzallan14@gmail.com>
2023-06-22 13:17:36 +0100

    Projects-display-desktop

--- portfolio23/src/components/SelectedProjects/Projects.jsx
+++ portfolio23/src/components/SelectedProjects/Projects.jsx
@@ -7,6 +7,9 @@ import {
   ProjectsWrapper,
 } from "./styledProjects";
 import DailyStoic from "../../../src/assets/images/project-images/daily-stoic.png";
+import ankan from "../../../src/assets/images/project-images/ankan.png";
+import apex from "../../../src/assets/images/project-images/apex-academy.png";
+import trent from "../../../src/assets/images/project-images/trent.png";
 import SingleProject from './SingleProject';
 
 function Projects() {
@@ -29,8 +32,24 @@ function Projects() {
           ProjectTitle="The Common Thread"
           onMouseEnter={() => setBlinky(true)}
           onMouseLeave={() => setBlinky(false)}
-          ProjectImage={DailyStoic}
-          ProjectDescription="Website design & development for @ankanchittalipi."
+          ProjectImage={ankan}
+          ProjectDescription="Website design & development for @ankanchittalipi, using wordpress as a headless CMS with a react frontend"
+        />
+        <SingleProject
+          ProjectUrl="http://trentfreshers.org/"
+          ProjectTitle="Trent Freshers "
+          onMouseEnter={() => setBlinky(true)}
+          onMouseLeave={() => setBlinky(false)}
+          ProjectImage={trent}
+          ProjectDescription="Website design development for @trentsu using wordpress"
+        />
+        <SingleProject
+          ProjectUrl="https://apexlearningacademy.co.in/"
+          ProjectTitle="Apex Learning Academy"
+          onMouseEnter={() => setBlinky(true)}
+          onMouseLeave={() => setBlinky(false)}
+          ProjectImage={apex}
+          ProjectDescription="Website design & development for @apexacademy using React"
         />
       </ProjectsWrapper>
     </ProjectsContainer>
