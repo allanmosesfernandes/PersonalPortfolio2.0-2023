7dc619c8ebd423158eef3cdec0f6d88c291946fe
Allan Fernandes <Fernzallan14@gmail.com>
2023-06-23 18:02:24 +0100

    about-structure

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -1,8 +1,29 @@
 import React from "react";
+import { AboutContainer, AboutLeft, AboutRight } from "./about-styles";
+import nerd from "../../assets/images/home/nerd.png";
+import Arrow from "../../assets/images/about/arrow.svg";
+
 
 const AboutMe = () => {
   return (
-    <div>dfdfdf</div>
+    <AboutContainer className="wrapper">
+      <AboutLeft>
+        <img src={nerd} className="about__picture" />
+        <p>
+          Hi, I'm Allan.
+        </p>
+        <img src={Arrow} className="about__arrow" />
+        <p>
+          I'm a front-end developer born in Goa, raised in Mumbai and currently in Nottingham, UK pursuing my masters in computer science from NTU.
+        </p>
+        <p>
+          I am 26 years old and still find the word Lauda Lasson to be amusing.
+        </p>
+      </AboutLeft>
+      <AboutRight>
+        <h2>Contact</h2>
+      </AboutRight>
+    </AboutContainer>
   );
 };
 
--- src/components/AboutMe/about-styles.js
+++ src/components/AboutMe/about-styles.js
@@ -2,7 +2,42 @@ import styled from "styled-components";
 
 export const AboutContainer = styled.div`
     display: flex;
-    flex-direction: column;
     justify-content: center;
     align-items: center;
+    margin: 0 auto;
+    font-family: 'Fira Code', monospace;
+    font-size: var(--fs2);
 `;
+export const AboutLeft = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 60%;
+  margin: 0 2rem;
+  .about__picture {
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+    border: 1px solid white;
+    background-color: black;
+  }
+
+  .about__arrow {
+    width: 50px;
+  }
+  @media screen and (max-width: 768px) {
+    width: 100%;
+  }
+`;
+
+export const AboutRight = styled.div`
+    display: flex;
+    flex-direction: column; 
+    justify-content: center;    
+    width: 40%; 
+    margin: 0 2rem;
+    @media screen and (max-width: 768px) {
+        width: 100%;
+    }
+`;
+
+
