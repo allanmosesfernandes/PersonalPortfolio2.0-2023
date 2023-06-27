69ad8629165c47ef3e095dab3d281fa755ed7a54
Allan <fernzallan14@gmail.com>
2023-06-27 11:37:51 +0100

    Projects-Desktop-Complete

--- src/components/ProjectsComponent/ProjectComponent.jsx
+++ src/components/ProjectsComponent/ProjectComponent.jsx
@@ -3,24 +3,41 @@ import React from 'react';
 const ProjectComponent = ({ data }) => {
   console.log(data);
   return (
-    <table>
+    <table className='project__table'>
       <thead>
         <tr>
           <th>Year</th>
           <th>Project</th>
           <th>Built with</th>
-          <th>Link</th>
+          <th width="30%">Link</th>
         </tr>
       </thead>
       <tbody>
-        {data.map((project,index) => (
-          <tr key={index}>
-            <td>{project.Year}</td>
-            <td>{project.Title}</td>
-            <td>{project.builtWith}</td>
-            <td>{project.ProjectLink}</td>
-          </tr>
-        ))}
+        {data.map((project, index) => {
+          let year = project.Year.split("-")[0];
+          let url = project.ProjectLink;
+          let extractedURL = url.substring(url.indexOf("https://") + 8, url.indexOf("/", 8)); //extracting the domain name from the url
+          return (
+            <tr key={index}>
+              <td>{year}</td>
+              <td>{project.Title}</td>
+              <td>
+                {project.BuiltWith.map((item, index) => {
+                  return (
+                    <div key={index} className="built__with">
+                      {item}
+                    </div>
+                  );
+                })}
+              </td>
+              <td>
+                <a className="project__link" href={project.ProjectLink}>
+                  {extractedURL}
+                </a>
+              </td>
+            </tr>
+          );
+        })}
       </tbody>
     </table>
   );
--- src/components/ProjectsComponent/ProjectStyles.js
+++ src/components/ProjectsComponent/ProjectStyles.js
@@ -5,12 +5,48 @@ export const ProjectsContainer = styled.div`
   flex-direction: column;
   font-family: "Fira Code", monospace;
 
+  .project__table {
+    font-size: 1.5rem;
+    border-collapse: collapse;
+
+    thead {
+      text-align: left;
+    }
+    tr {
+      padding-bottom: 1rem;
+    }
+    th,
+    td {
+      border-bottom: 1px solid white;
+      padding: 1.5rem 2rem;
+    }
+
+    .project__link {
+      position: relative;
+      &:after {
+        //show top right arrow
+      position: absolute;
+      content: "↗";
+      right: -2rem;
+      top: -1rem;
+    }
+  }
+
+  .built__with {
+    margin-right: 5px;
+    border-radius: 5px;
+    background-color: var(--base-orange);
+    padding: 1rem 0.5rem;
+    opacity: 0.8;
+    display: inline-block;
+    margin: 1rem;
+  }
   .backLink {
     font-size: var(--fs2);
     color: var(--base-orange);
     &:before {
-        content: "←";
-        margin-right: 0.5rem;
+      content: "←";
+      margin-right: 0.5rem;
     }
   }
   h1 {
