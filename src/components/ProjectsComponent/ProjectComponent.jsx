ae9af47a09631a63e20e7f7ac1e0230810aeaf60
Allan <fernzallan14@gmail.com>
2023-06-27 12:00:06 +0100

    projects-mobile

--- src/components/ProjectsComponent/ProjectComponent.jsx
+++ src/components/ProjectsComponent/ProjectComponent.jsx
@@ -1,27 +1,31 @@
 import React from 'react';
+import { ProjectTable } from "./ProjectStyles";
 
 const ProjectComponent = ({ data }) => {
   console.log(data);
   return (
-    <table className='project__table'>
+    <ProjectTable>
       <thead>
         <tr>
           <th>Year</th>
-          <th>Project</th>
-          <th>Built with</th>
-          <th width="30%">Link</th>
+          <th className="mobile__hide">Project</th>
+          <th className="mobile__hide">Built with</th>
+          <th width="w-30">Link</th>
         </tr>
       </thead>
       <tbody>
         {data.map((project, index) => {
           let year = project.Year.split("-")[0];
           let url = project.ProjectLink;
-          let extractedURL = url.substring(url.indexOf("https://") + 8, url.indexOf("/", 8)); //extracting the domain name from the url
+          let extractedURL = url.substring(
+            url.indexOf("https://") + 8,
+            url.indexOf("/", 8)
+          ); //extracting the domain name from the url
           return (
             <tr key={index}>
               <td>{year}</td>
-              <td>{project.Title}</td>
-              <td>
+              <td className="mobile__hide">{project.Title}</td>
+              <td className="mobile__hide">
                 {project.BuiltWith.map((item, index) => {
                   return (
                     <div key={index} className="built__with">
@@ -39,7 +43,7 @@ const ProjectComponent = ({ data }) => {
           );
         })}
       </tbody>
-    </table>
+    </ProjectTable>
   );
 };
 
--- src/components/ProjectsComponent/ProjectStyles.js
+++ src/components/ProjectsComponent/ProjectStyles.js
@@ -1,36 +1,10 @@
+import { Link } from "gatsby";
 import styled from "styled-components";
 
 export const ProjectsContainer = styled.div`
   display: flex;
   flex-direction: column;
   font-family: "Fira Code", monospace;
-
-  .project__table {
-    font-size: 1.5rem;
-    border-collapse: collapse;
-
-    thead {
-      text-align: left;
-    }
-    tr {
-      padding-bottom: 1rem;
-    }
-    th,
-    td {
-      border-bottom: 1px solid white;
-      padding: 1.5rem 2rem;
-    }
-
-    .project__link {
-      position: relative;
-      &:after {
-        //show top right arrow
-      position: absolute;
-      content: "↗";
-      right: -2rem;
-      top: -1rem;
-    }
-  }
 
   .built__with {
     margin-right: 5px;
@@ -52,4 +26,54 @@ export const ProjectsContainer = styled.div`
   h1 {
     font-size: 3rem;
   }
+}
 `;
+
+export const BackLink = styled(Link)`
+    font-size: var(--fs2);
+    margin-bottom: 4rem;
+    color: var(--base-orange);
+    &:before {
+      content: "←";
+      margin-right: 0.5rem;
+    }
+`;
+
+
+export const ProjectTable = styled.table`
+  font-size: 1.5rem;
+  border-collapse: collapse;
+  text-align: left;
+
+  thead {
+    text-align: left;
+  }
+
+  tr {
+    padding-bottom: 1rem;
+  }
+
+  th,
+  td {
+    border-bottom: 1px solid white;
+    padding: 1.5rem 2rem;
+  }
+
+  .project__link {
+    position: relative;
+
+    &:after {
+      content: "↗";
+      position: absolute;
+      right: -2rem;
+      top: -1rem;
+    }
+  }
+
+  @media screen and (max-width: 768px) {
+    .mobile__hide {
+      display: none;
+    }
+  }
+`;
+
