aa14e22c3c04f0f039e9d9a0b10cde9e61631404
Allan Fernandes <Fernzallan14@gmail.com>
2023-06-28 01:01:15 +0100

    Blog-bg

--- src/components/BlogComponents/BlogStyles.js
+++ src/components/BlogComponents/BlogStyles.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import BlogBackground from "../../assets/images/blog/blog-bg.png";
 
 export const BlogListContainer = styled.div`
     display: flex;
@@ -8,6 +9,15 @@ export const BlogListContainer = styled.div`
     margin: 2rem 0;
     padding: 2rem 0;
     width: 100%;
-
 `;
 
+export const FullWidthBlogBg = styled.div`
+    background: #000;
+    width: 100%;
+    background-image: url(${BlogBackground});
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    height: 300px;
+    z-index: -1;
+`;
--- src/components/BlogComponents/FeaturedBlogs.js
+++ src/components/BlogComponents/FeaturedBlogs.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import {TwitterTweetEmbed} from "react-twitter-embed";
-import { BlogListContainer } from './BlogStyles';
+import { BlogListContainer, FullWidthBlogBg } from './BlogStyles';
 
 const FeaturedBlogs = ({ data }) => {
   console.log(data);
   return (
-    <BlogListContainer className='wrapper'>
-      <TwitterTweetEmbed tweetId={"1672156965693272066"} />
-    </BlogListContainer>
+    
+      <BlogListContainer className="wrapper">
+        <TwitterTweetEmbed tweetId={"1672156965693272066"} />
+      </BlogListContainer>
   );
 };
 
