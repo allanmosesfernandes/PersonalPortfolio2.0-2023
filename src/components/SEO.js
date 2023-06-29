909846af747dd464eae71c8ce4295844dbcc88a4
Allan Fernandes <Fernzallan14@gmail.com>
2023-06-29 14:49:59 +0100

    og-image

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Helmet } from "react-helmet";
 import { useStaticQuery, graphql } from "gatsby";
-
+import OGImage from "../assets/images/alt/OGIMAGE.png";
 const SEO = ({ children, location, description, title, image}) => {
 //write static query to fetch data from graphql
 //use siteMetadata to get data
@@ -23,15 +23,19 @@ const SEO = ({ children, location, description, title, image}) => {
       <html lang="en" />
       <title>{title}</title>
       {/* Meta Tags*/}
-      <meta name="description" content={site.siteMetadata.description}/>
+      <meta name="description" content={site.siteMetadata.description} />
       <meta name="theme-color" content="#F9A600" />
       {/* Open Graph */}
       {location && <meta property="og:url" content={location.href} />}
-      <meta property="og:image" content={image || "/logo.png"} />
+      <meta property="og:image" content={OGImage || "/logo.png"} />
       <meta property="og:title" content={title} key="ogtitle" />
-      <meta property="og:site_name" content={site.siteMetadata.title} key="ogsitename" />
+      <meta
+        property="og:site_name"
+        content={site.siteMetadata.title}
+        key="ogsitename"
+      />
       <meta property="og:description" content={description} key="ogdesc" />
-      { children }
+      {children}
     </Helmet>
   );
 }; 
