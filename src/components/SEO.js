dca9cd340987ca9c0bcebf2d5bcb91d825e60212
Allan Fernandes <Fernzallan14@gmail.com>
2023-06-29 14:39:35 +0100

    SEojs

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -31,6 +31,7 @@ const SEO = ({ children, location, description, title, image}) => {
       <meta property="og:title" content={title} key="ogtitle" />
       <meta property="og:site_name" content={site.siteMetadata.title} key="ogsitename" />
       <meta property="og:description" content={description} key="ogdesc" />
+      { children }
     </Helmet>
   );
 }; 
